2a12543be829b5924f00be10e60d57773394d3eb
Joselak <joseignaciobravo97@gmail.com>
2025-10-13 23:17:20 -0400

    terminadas las modificaciones en la actividad 10

--- Actividad10/Facultad.java
+++ Actividad10/Facultad.java
@@ -1,6 +1,9 @@
 package Actividad10;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -24,6 +27,77 @@ public class Facultad {
         }
 
         return lista;
+    }
+
+
+    //Inciso d
+    public ArrayList<String> obtenerNombres(String asignatura, int edad) throws ClassNotFoundException, IOException{
+
+        ArrayList<String>devolver = new ArrayList<>();
+        RandomAccessFile raf = new RandomAccessFile(fichero, "r");
+
+        int cantP = raf.readInt();
+
+        for (int i = 0; i < cantP; i++) {
+            int tamP = raf.readInt();
+            byte[] ProfeArray = new byte[tamP];
+            raf.read(ProfeArray);
+            Profesor p = (Profesor)Convert.toObject(ProfeArray);
+
+            if(p.getAsignatura().equals(asignatura) && p.getEdad()==edad){
+                devolver.add(p.getNombre());
+            }
+        }
+        raf.close();
+
+        return devolver;
+    }
+
+
+    //Inciso e
+    public boolean verificarExistencia(RandomAccessFile raf,Profesor a) throws IOException, ClassNotFoundException{
+        boolean existe = false;
+        int i = 0;
+
+        int cantP = raf.readInt();
+
+        while(i < cantP && !existe) {
+            int tamPr = raf.readInt();
+            byte[] tam = new byte[tamPr];
+            raf.read(tam);
+            Profesor p = (Profesor)Convert.toObject(tam);
+
+            if (p.getNombre().equals(a.getNombre())) {
+                existe = true;
+            }
+
+            i++;
+        }
+        raf.close();
+
+        return existe;
+    }
+
+
+    public boolean anadirProfe(Profesor a) throws ClassNotFoundException, IOException{
+        boolean hecho = true;
+        RandomAccessFile raf = new RandomAccessFile(fichero, "rw");
+
+        raf.seek(0);
+        int cant = raf.readInt();
+
+        if(!verificarExistencia(raf, a)){
+            raf.seek(raf.length());
+            byte[] profe = Convert.toBytes(a);
+            raf.writeInt(profe.length);
+            raf.write(profe);
+            raf.seek(0);
+            raf.write(cant++);
+        }else{
+            hecho = false;
+        }
+
+        return hecho;
     }
 
 }
