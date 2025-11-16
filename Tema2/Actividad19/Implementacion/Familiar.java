9a8f0c6d4f194b52452f28227688b6605d409588
Joselak <joseignaciobravo97@gmail.com>
2025-11-16 12:40:55 -0500

    Trabajando en la actividad 19.

---
+++ Tema2/Actividad19/Implementacion/Familiar.java
@@ -0,0 +1,39 @@
+public class Familiar {
+    private String nombre;
+    private int edad;
+    private int lineaSucecion;
+
+    public Familiar(String nombre, int edad, int lineaSucecion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.lineaSucecion = lineaSucecion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getLineaSucecion() {
+        return lineaSucecion;
+    }
+
+    public void setLineaSucecion(int lineaSucecion) {
+        this.lineaSucecion = lineaSucecion;
+    }
+
+    
+    
+
+}
---
+++ Tema2/Actividad19/Implementacion/Mundo.java
@@ -0,0 +1,154 @@
+import java.util.Iterator;
+import java.util.List;
+
+import cu.edu.cujae.ceis.tree.binary.BinaryTreeNode;
+import cu.edu.cujae.ceis.tree.general.GeneralTree;
+import cu.edu.cujae.ceis.tree.iterators.general.InBreadthIterator;
+
+public class Mundo {
+    GeneralTree<Object>ponienteTree;
+
+    public Mundo(GeneralTree<Object> ponienteTree) {
+        this.ponienteTree = new GeneralTree<>();
+    }
+
+    //Inciso b /================================================================
+    public String mayorCantBatGanadas(String reino){
+        String casa = " ";
+        boolean encontrado = false;
+
+        InBreadthIterator<Object>iter = ponienteTree.inBreadthIterator();
+
+        while (iter.hasNext() && !encontrado) {
+            BinaryTreeNode<Object>nodo = iter.nextNode();
+            int mayor = 0;
+
+            if (nodo.getInfo() instanceof Reino) {
+                Reino r = (Reino)nodo.getInfo();
+                if (r.getNombre().equals(reino)) {
+                    encontrado = true;
+                    List<BinaryTreeNode<Object>>sons = (List<BinaryTreeNode<Object>>)ponienteTree.getSons(nodo);
+
+                    for (Object c : sons) {
+                        if (((CasaNoble)c).getBatallasGanadas()>mayor) {
+                            casa = ((CasaNoble)c).getNombre();
+                        }
+                    }
+                }
+            }
+            nodo = iter.nextNode();
+        }
+
+        return casa;
+    }
+
+
+    //Inciso c /================================================================
+    public boolean traslado(String deja,String recibe){
+        boolean hecho = false;
+        boolean eliminado ;
+        boolean agregado;
+        Reino reinoD = null;
+        CasaNoble casa = null;
+
+        InBreadthIterator<Object>iter = ponienteTree.inBreadthIterator();
+
+        casa = buscarMayorCantBatallas(iter,deja,reinoD);
+
+        eliminado = eliminarCasa(reinoD,casa);
+
+        agregado = agregarAlNuevoReino(iter, casa, recibe);
+
+        if (eliminado && agregado) {
+            hecho = true;
+        }
+
+
+        return hecho;
+    }
+
+    //Metodo para buscar la casa con mas batallas
+    public CasaNoble buscarMayorCantBatallas(InBreadthIterator<Object>iter,String deja,Reino reinoD){
+        CasaNoble ca = null;
+        boolean encontrado = false;
+
+        while (iter.hasNext() && !encontrado) {
+            BinaryTreeNode<Object>nodo = iter.nextNode();
+            int mayor = 0;
+
+            if (nodo.getInfo() instanceof Reino) {
+                if(((Reino)nodo.getInfo()).getNombre().equals(deja)){
+                    encontrado = true;
+                    List<BinaryTreeNode<Object>>sons = (List<BinaryTreeNode<Object>>)ponienteTree.getSons(nodo);
+                    for (Object c : sons) {
+                        if (((CasaNoble)c).getBatallasGanadas()>mayor) {
+                            ca = (CasaNoble)c;
+                        }
+                    }
+                    reinoD = ((Reino)nodo.getInfo());
+
+                }
+            }
+            nodo = iter.nextNode();
+        }
+
+        return ca;
+    }
+
+    //Metodo para agegar la casa al  nuevo reino
+    public boolean agregarAlNuevoReino(InBreadthIterator<Object>iter,CasaNoble casa,String recibe) {
+        boolean encontrado = false;
+        boolean hecho = false;
+        Reino r = null;
+
+        while (iter.hasNext() && !encontrado) {
+            BinaryTreeNode<Object>nodo = iter.nextNode();
+        
+            if (nodo.getInfo() instanceof Reino) {
+                if(((Reino)nodo.getInfo()).getNombre().equals(recibe)){
+                    encontrado = true;
+                    r = (Reino)nodo.getInfo();
+                    r.getCasas().add(casa); // agrego la casa al nuevo reino
+                }
+            }
+            nodo = iter.nextNode();
+        }
+
+        if (r.getCasas().contains(casa)) {
+            hecho = true;
+        }
+
+        return hecho;
+    }
+
+    //Metodo para eliminar una casa
+    public boolean eliminarCasa(Reino reino,CasaNoble casa){
+        boolean hecho = false;
+        boolean stop = false;
+        String nombre;
+
+        Iterator<CasaNoble>iter = reino.getCasas().iterator(); 
+
+        while (iter.hasNext() && !stop) {
+            CasaNoble c = iter.next();
+            nombre = c.getNombre();
+            
+            if (casa.getNombre().equals(nombre)){
+                iter.remove();
+                stop = true;
+            }
+        }
+
+        if(!(reino.getCasas().contains(casa))){
+            hecho = true;
+        }
+
+        return hecho;
+    }
+
+
+
+    //Inciso d /====================================================
+
+
+}
